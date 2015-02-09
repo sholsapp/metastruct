834f4235b892c1810a63cad5d94a0b2ee42586b3
Stephen Holsapple <sholsapp@gmail.com>
2015-02-08 18:35:36 -0800

    Description update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
   name='metastruct',
   version='0.0.1',
-  description="Just another library.",
+  description="A library for untangling the stringy mess that usually comes with communicating with JSON API in the wild.",
   author='Stephen Holsapple',
   author_email='sholsapp@gmail.com',
   url='https://github.com/sholsapp/metastruct',
